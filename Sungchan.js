417e36b8a978936a0bfa8578660ccc400aaaa34a
Rhosungchan <110643104+Rhosungchan@users.noreply.github.com>
2023-04-16 11:40:40 +0900

    Delete Sungchan.js

--- Sungchan.js
+++
@@ -1,8 +0,0 @@
-function enterTime() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const time = `${hours}:${minutes}:${seconds}`;
-    document.getElementById("time").innerHTML = time;
-}
